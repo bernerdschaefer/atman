ef492d1683fc751365d2887632f7b67040797594
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-11-05 18:49:08 -0800

    Add dummy sysAlloc and sysReserve

--- src/runtime/mem_atman.go
+++ src/runtime/mem_atman.go
@@ -2,10 +2,18 @@ package runtime
 
 import "unsafe"
 
-func sysAlloc(n uintptr, sysStat *uint64) unsafe.Pointer                    { return nil }
-func sysFree(v unsafe.Pointer, n uintptr, sysStat *uint64)                  {}
-func sysMap(v unsafe.Pointer, n uintptr, reserved bool, sysStat *uint64)    {}
-func sysReserve(v unsafe.Pointer, n uintptr, reserved *bool) unsafe.Pointer { return nil }
-func sysUnused(v unsafe.Pointer, n uintptr)                                 {}
-func sysUsed(v unsafe.Pointer, n uintptr)                                   {}
-func sysFault(v unsafe.Pointer, n uintptr)                                  {}
+func sysFree(v unsafe.Pointer, n uintptr, sysStat *uint64)               {}
+func sysMap(v unsafe.Pointer, n uintptr, reserved bool, sysStat *uint64) {}
+func sysUnused(v unsafe.Pointer, n uintptr)                              {}
+func sysUsed(v unsafe.Pointer, n uintptr)                                {}
+func sysFault(v unsafe.Pointer, n uintptr)                               {}
+
+func sysAlloc(n uintptr, sysStat *uint64) unsafe.Pointer {
+	mSysStatInc(sysStat, n)
+	return unsafe.Pointer(n)
+}
+
+func sysReserve(v unsafe.Pointer, n uintptr, reserved *bool) unsafe.Pointer {
+	*reserved = true
+	return unsafe.Pointer(v)
+}
