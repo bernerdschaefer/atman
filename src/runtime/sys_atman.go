9ed4f89c450a05ac06fee884b1f0bfbc9e120f09
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-11-14 17:22:24 -0800

    Spike on machinery for page table building

--- src/runtime/sys_atman.go
+++ src/runtime/sys_atman.go
@@ -172,34 +172,10 @@ func atmaninit() {
 		_atman_start_info.MfnList,
 	))
 
-	printPageDirectory()
-
 	println("mapping _atman_start_info")
 	mapSharedInfo(_atman_start_info.SharedInfoAddr, _atman_shared_info)
 
 	buildPageTable()
-}
-
-func printPageDirectory() {
-	var table xenPageDirectory
-	table = *(*xenPageDirectory)(unsafe.Pointer(uintptr(
-		_atman_start_info.PageTableBase,
-	)))
-
-	for _, x := range table {
-		if x.hasFlag(xenPageTablePresent) {
-			x.debug()
-			println("pte2virt: ", pteToVirt(x))
-		}
-	}
-}
-
-func pteToVirt(e pageTableEntry) unsafe.Pointer {
-	const (
-		m2p xenMachineToPhysicalMap = 0xFFFF800000000000
-	)
-
-	return unsafe.Pointer(m2p.Get(e.Mfn()) << 12)
 }
 
 func mapSharedInfo(vaddr uintptr, i *xenSharedInfo) {
@@ -247,6 +223,27 @@ func HYPERVISOR_update_va_mapping(vaddr uintptr, val uintptr, flags uint64) uint
 		val,
 		uintptr(flags),
 		0,
+		0,
+		0,
+	)
+}
+
+type mmuUpdate struct {
+	ptr uintptr // machine address of PTE
+	val uintptr // contents of new PTE
+}
+
+const DOMID_SELF = 0x7FF0
+
+func HYPERVISOR_mmu_update(updates []mmuUpdate, domid uint16) uintptr {
+	const _HYPERVISOR_mmu_update = 1
+
+	return hypercall(
+		_HYPERVISOR_mmu_update,
+		uintptr(unsafe.Pointer(&updates[0])),
+		uintptr(len(updates)),
+		0, // done_out (unused)
+		uintptr(domid),
 		0,
 		0,
 	)
@@ -302,7 +299,7 @@ const (
 func (e pageTableEntry) debug() {
 	println(
 		"PTE<", unsafe.Pointer(e), ">:",
-		" MFN=", e.Mfn(),
+		" MFN=", e.mfn(),
 		"  NX=", e.hasFlag(xenPageTableNoExecute),
 		"   G=", e.hasFlag(xenPageTableGlobal),
 		" PAT=", e.hasFlag(xenPageTablePAT),
@@ -316,31 +313,73 @@ func (e pageTableEntry) debug() {
 	)
 }
 
+func (e *pageTableEntry) setFlag(f uintptr) {
+	*e = pageTableEntry(uintptr(*e) | f)
+}
+
 func (e pageTableEntry) hasFlag(f uintptr) bool {
 	return uintptr(e)&f == f
 }
 
-func (e pageTableEntry) Mfn() uintptr {
-	return (uintptr(e) & (xenPageAddrMask &^ xenPageMask)) >> xenPageFlagShift
+func (e pageTableEntry) mfn() mfn {
+	return mfn((uintptr(e) & (xenPageAddrMask &^ xenPageMask)) >> xenPageFlagShift)
 }
 
-type (
-	xenPageDirectory [512]pageTableEntry
-	xenPageTable     [512]pageTableEntry
-)
+func (e pageTableEntry) vaddr() vaddr {
+	const (
+		m2p xenMachineToPhysicalMap = 0xFFFF800000000000
+	)
+
+	return vaddr(m2p.Get(e.mfn()) << 12)
+}
+
+type xenPageTable [512]pageTableEntry
+
+func newXenPageTable(vaddr vaddr) xenPageTable {
+	return *(*xenPageTable)(unsafe.Pointer(vaddr))
+}
 
 type xenMachineToPhysicalMap uintptr
 
-func (m xenMachineToPhysicalMap) Get(i uintptr) uintptr {
-	return *(*uintptr)(add(unsafe.Pointer(m), i*ptrSize))
+func (m2p xenMachineToPhysicalMap) Get(mfn mfn) pfn {
+	offset := uintptr(mfn) * ptrSize
+
+	return pfn(*(*uintptr)(add(unsafe.Pointer(m2p), offset)))
 }
 
-type vaddr uintptr
+type pageTableLevel int
 
-func (a vaddr) pfn() pfn {
-	const virtStart = 0x401000 // TODO: don't hard code this...
+func (l pageTableLevel) shift() uint64 {
+	return 12 + uint64(l)*9
+}
 
-	return pfn((uint64(a) - virtStart + _PAGESIZE - 1) >> 12)
+// mask returns a mask for the pageTableLevel l.
+// It's undefined if l is pageTableLevel4.
+func (l pageTableLevel) mask() uint64 {
+	return (1 << (l + 1).shift()) - 1
+}
+
+const (
+	pageTableLevel1 pageTableLevel = iota
+	pageTableLevel2
+	pageTableLevel3
+	pageTableLevel4
+)
+
+func (a vaddr) pageTableOffset(level pageTableLevel) int {
+	return int((a >> level.shift()) & (512 - 1))
+}
+
+// requiredPageTables returns the number of page tables
+// required to map the complete address space.
+func (a vaddr) requiredPageTables() uint64 {
+	var (
+		l3tables = uint64(a)/pageTableLevel3.mask() + 1
+		l2tables = uint64(a)/pageTableLevel2.mask() + 1
+		l1tables = uint64(a)/pageTableLevel1.mask() + 1
+	)
+
+	return l3tables + l2tables + l1tables
 }
 
 type pfn uint64
@@ -355,16 +394,100 @@ func (n pfn) add(v uint64) pfn {
 	return n + pfn(v)
 }
 
-func buildPageTable() {
-	var (
-		basePFN  = _atman_start_info.PageTableBase.pfn()
-		startPFN = basePFN.add(_atman_start_info.NrPageTableFrames)
-		endPFN   = pfn(_atman_start_info.NrPages)
+func (n pfn) mfn() mfn {
+	return mfn(_atman_phys_to_machine_mapping[n])
+}
 
-		startAddress = startPFN.vaddr()
-		endAddress   = endPFN.vaddr()
+type mfn uintptr
+
+func (m mfn) pfn() pfn {
+	const (
+		m2p xenMachineToPhysicalMap = 0xFFFF800000000000
 	)
 
-	println("startPFN = ", startPFN, "endPFN = ", endPFN)
-	println("startAddress = ", startAddress, "endAddress = ", endAddress)
+	return pfn(m2p.Get(m))
+}
+
+type vaddr uintptr
+
+func (a vaddr) pfn() pfn {
+	const virtStart = 0x401000 // TODO: don't hard code this...
+
+	return pfn((uint64(a) - virtStart + _PAGESIZE - 1) >> 12)
+}
+
+func buildPageTable() {
+	var (
+		basePFN = _atman_start_info.PageTableBase.pfn()
+
+		nextPFN = basePFN.add(_atman_start_info.NrPageTableFrames + 1)
+
+		endPFN     = pfn(_atman_start_info.NrPages)
+		endAddress = endPFN.vaddr()
+
+		startPFN     = basePFN.add(endAddress.requiredPageTables())
+		startAddress = startPFN.vaddr()
+	)
+
+	println(
+		"basePFN = ", basePFN,
+		"nextPFN = ", nextPFN,
+		"startPFN = ", startPFN,
+		"endPFN = ", endPFN,
+		"startAddress = ", startAddress,
+		"endAddress = ", endAddress,
+	)
+
+	for startAddress < endAddress {
+		var (
+			l4offset = startAddress.pageTableOffset(pageTableLevel4)
+			l3offset = startAddress.pageTableOffset(pageTableLevel3)
+			l2offset = startAddress.pageTableOffset(pageTableLevel2)
+			// l1offset = startAddress.pageTableOffset(pageTableLevel1)
+
+			l4 = newXenPageTable(_atman_start_info.PageTableBase)
+		)
+
+		if uint64(startAddress)&pageTableLevel3.mask() == 0 && !l4[l4offset].hasFlag(xenPageTablePresent) {
+			panic("NOT YET IMPLEMENTED")
+		}
+
+		l3 := newXenPageTable(l4[l4offset].vaddr())
+
+		if uint64(startAddress)&pageTableLevel2.mask() == 0 && !l3[l3offset].hasFlag(xenPageTablePresent) {
+			panic("NOT YET IMPLEMENTED")
+		}
+
+		l2 := newXenPageTable(l3[l3offset].vaddr())
+
+		if uint64(startAddress)&pageTableLevel1.mask() == 0 && !l2[l2offset].hasFlag(xenPageTablePresent) {
+			memclr(unsafe.Pointer(nextPFN.vaddr()), _PAGESIZE) // clear page to mapping as table
+
+			newpte := pageTableEntry(nextPFN.mfn() << xenPageFlagShift)
+			newpte.setFlag(xenPageTablePresent | xenPageTableDirty | xenPageTableAccessed | xenPageTableUserspaceAccessible)
+
+			pfnOffset2 := nextPFN.vaddr().pageTableOffset(pageTableLevel2)
+			pfnOffset1 := nextPFN.vaddr().pageTableOffset(pageTableLevel1)
+
+			updates := []mmuUpdate{
+				{
+					ptr: uintptr((l2[pfnOffset2].mfn() << xenPageFlagShift)) + uintptr(pfnOffset1*ptrSize),
+					val: uintptr(newpte),
+				},
+			}
+
+			ret := HYPERVISOR_mmu_update(updates, DOMID_SELF)
+
+			if ret != 0 {
+				println("HYPERVISOR_mmu_update returned ", ret)
+				panic("HYPERVISOR_mmu_update failed")
+			}
+
+			nextPFN += 1
+		}
+
+		startAddress += _PAGESIZE
+	}
+
+	println("END")
 }
--- src/runtime/sys_atman_amd64.s
+++ src/runtime/sys_atman_amd64.s
@@ -45,7 +45,7 @@ TEXT runtime·hypercall(SB),NOSPLIT,$0
 	MOVQ	a1+8(FP), DI
 	MOVQ	a2+16(FP), SI
 	MOVQ	a3+24(FP), DX
-	MOVQ	a4+32(FP), CX
+	MOVQ	a4+32(FP), R10
 	MOVQ	a5+40(FP), R8
 	MOVQ	a6+48(FP), R9
 	HYPERCALL(trap+0(FP))
