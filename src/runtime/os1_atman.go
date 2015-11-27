e4f4b29ed2b90352bf505e0fb26fdab73a9bb386
Bernerd Schaefer <bj.schaefer@gmail.com>
2015-11-27 11:51:00 -0800

    Use lock_sema for atman

--- src/runtime/os1_atman.go
+++ src/runtime/os1_atman.go
@@ -40,3 +40,26 @@ func msigsave(mp *m) {
 func osyield() {
 	println("osyield()")
 }
+
+// Create a semaphore, which will be assigned to m->waitsema.
+// The zero value is treated as absence of any semaphore,
+// so be sure to return a non-zero value.
+//go:nosplit
+func semacreate() uintptr {
+	println("semacreate()")
+	return 1
+}
+
+// If ns < 0, acquire m->waitsema and return 0.
+// If ns >= 0, try to acquire m->waitsema for at most ns nanoseconds.
+// Return 0 if the semaphore was acquired, -1 if interrupted or timed out.
+//go:nosplit
+func semasleep(ns int64) int32 {
+	return 0
+}
+
+// Wake up mp, which is or will soon be sleeping on mp->waitsema.
+//go:nosplit
+func semawakeup(mp *m) int32 {
+	return 0
+}
--- src/runtime/sys_atman.go
+++ src/runtime/sys_atman.go
@@ -19,22 +19,6 @@ func getRandomData(r []byte) {
 	extendRandom(r, 0)
 }
 
-// lock
-
-const (
-	active_spin     = 4
-	active_spin_cnt = 30
-)
-
-func lock(l *mutex)   {}
-func unlock(l *mutex) {}
-
-func noteclear(n *note)                  {}
-func notewakeup(n *note)                 {}
-func notesleep(n *note)                  {}
-func notetsleep(n *note, ns int64) bool  { return false }
-func notetsleepg(n *note, ns int64) bool { return false }
-
 // env
 
 func gogetenv(key string) string { return "" }
